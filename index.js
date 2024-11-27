5e93037fa3e3e3c6d2f102f0bcc7c83b404cbccc
Alex Malkov <malkov.fighter@yandex.ru>
2024-11-27 18:21:08 +0700

    New ws client

--- index.js
+++ index.js
@@ -1,71 +1,21 @@
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const WebSocket = require("ws");
+
 const app = express();
-const path = require('path');
 
+const server = http.createServer(app);
 
+const webSocketServer = new WebSocket.Server({ server });
 
-const ws = require('ws');
-
-
-
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
-})
-
-app.listen(process.env.PORT || 3000);
-
-
-
-
-
-
-
-const wsServer = new ws.Server({ port: 4000 });
-
-function onConnect(wsClient) {
-    console.log('New client');
-
-    wsClient.send('Hellow!');
-
-    wsClient.on('message', function (message) {
-        try {
-
-            const jsonMessage = JSON.parse(message);
-
-            console.log(jsonMessage);
-
-            switch (jsonMessage.action) {
-                case 'ECHO':
-                    wsClient.send(jsonMessage.data);
-                    break;
-                case 'PING':
-                    setTimeout(function () {
-                        wsClient.send('PONG');
-                    }, 1000);
-                    break;
-                default:
-                    console.log('Unknown command');
-                    break;
-            }
-
-        } catch (error) {
-            console.log('Error', error);
-        }
+webSocketServer.on('connection', ws => {
+    ws.on('message', m => {
+        webSocketServer.clients.forEach(client => client.send(m));
     });
 
-    wsClient.on('close', function () {
+    ws.on("error", e => ws.send(e));
 
-        console.log('Client disconnected');
-    });
-}
+    ws.send('Hi there, I am a WebSocket server');
+});
 
-wsServer.on('connection', onConnect);
-
-
-
-
-
-
-module.exports = app;
+server.listen(4000, () => console.log("Server started"))
